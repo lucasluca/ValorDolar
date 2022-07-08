53c2c80d2ea17a682be7474f65ac39e8fb68b6a6
Lucas Mendes <lucas.augustus@gmail.com>
2022-07-08 20:36:44 -0300

    Initial Commit

---
+++ ValorDolar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValorDolar
+//
+//  Created by Lucas Mendes on 08/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ValorDolar/ValorDolarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorDolarApp.swift
+//  ValorDolar
+//
+//  Created by Lucas Mendes on 08/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ValorDolarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
