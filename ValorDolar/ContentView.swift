082e53bea58bc4e44ee03b74952e676b61fb96a8
Lucas Mendes <lucas.augustus@gmail.com>
2022-07-10 11:55:24 -0300

    feat: Initial commit with basic feature

--- ValorDolar/ContentView.swift
+++ ValorDolar/ContentView.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import ServiceManagement
 
 struct ContentView: View {
+    @AppStorage("checkedStored") var checkedStored: String = "true"
+    @State private var checked = false
+    let helperBundleIdentifier = "LucasMendes.ValorDolar"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Toggle("Start at login", isOn: $checked)
+                .toggleStyle(.checkbox)
+                .onChange(of: checked) { value in
+                    let state = SMLoginItemSetEnabled(helperBundleIdentifier as CFString, checked)
+                    print("Setting is enabled \(state)")
+                    checkedStored = String(value)
+                }
+                .onAppear {
+                    if checkedStored == "true" {
+                        self.checked = true
+                    } else {
+                        self.checked = false
+                    }
+                }
+            Spacer()
+        }
+        .padding(.leading, 15)
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- ValorDolar/ValorDolarApp.swift
+++ ValorDolar/ValorDolarApp.swift
@@ -9,9 +9,59 @@ import SwiftUI
 
 @main
 struct ValorDolarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    static private(set) var instance: AppDelegate!
+    lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let menu = ApplicationMenu()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        AppDelegate.instance = self
+        statusBarItem.button?.title = "Loading.."
+        statusBarItem.menu = menu.createMenu()
+        fetch()
+        Timer.scheduledTimer(withTimeInterval: 60, repeats: true) {
+            (_) in
+            self.fetch()
+        }
+    }
+    
+    
+    func fetch() {
+        Task {
+            await self.getDolarValueFromApi()
+        }
+    }
+    
+    func changeStatusBarValue(_ value: String) {
+        DispatchQueue.main.async {
+            if(value == "error") {
+                self.statusBarItem.button?.title = "Error"
+            } else {
+                self.statusBarItem.button?.title = "R$ \(value)"
+            }
+        }
+    }
+    
+    func getDolarValueFromApi() async {
+        let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+        let apiService = APIService(urlString: url)
+        do {
+            let response: EconomiaApiResponse = try await apiService.getJSON()
+            let coinFloat = Float(response.usdbrl.ask) ?? 0
+            let roundValue = String(format:"%.2f", coinFloat)
+            changeStatusBarValue(roundValue)
+        } catch {
+            print("error: \(error)")
+            changeStatusBarValue("error")
+        }
+    }
+    
 }
